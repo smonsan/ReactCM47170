f799e3a2d2e35bc3a545366ba49a9088c39e4f70
smonsalvo <sergio.monsalvo.s@gmail.com>
2023-09-17 15:33:17 -0300

    agregando la funcion custom context para darle funcionalidad al compoente carrito

---
+++ src/Components/Context/CartContext.js
@@ -0,0 +1,48 @@
+import { createContext, useState } from "react"
+
+
+ export const CartContext = createContext ({
+
+cart : []
+ })
+  
+
+ export const CartProvider = ({ children}) => {
+    const [cart,setCart] = useState ([])
+
+    console.log(cart)
+
+    const addItem = (item, quantity) => {
+
+        if(!isinCart(item.id)) {
+            setCart (prev => [...prev, {...item, quantity}])
+        } else {
+                console.error ("el producto ya fue agregado")
+            }
+        }
+
+
+    const removeItem = (itemId) => {
+        const cartUpdated = cart.filter (prod.id !== itemId)
+        setCart(cartUpdated)
+    }
+
+    const clearCart = () => {
+ setCart ([])
+    }
+
+    const insInCart = (itemId) => {
+        return cart.some (prod => prod.id ===itemId)
+    }
+
+    return (
+        <CartContext.Provider value = {{ cart, addItem,removeItem, clearCart}}>
+        {children}
+        </CartContext.Provider>
+    
+        )
+
+
+ }
+
+export default CartContext
--- src/Components/ItemDetail/ItemDetail.js
+++ src/Components/ItemDetail/ItemDetail.js
@@ -1,23 +1,39 @@
 import ItemCount from "../ItemCount/ItemCount"
-
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 const ItemDetail = ({ id, title, image, description, price, stock }) => {
+
+    const [quantityAdded, setQuantityAdded] = useState(0)
+
+    const handleOnAdd = (quantity) => {
+        setQuantityAdded(quantity)
+    }
+
     return (
-        
+
         <div className="card">
-        <img src={image} className="card-img-top w-25" alt="title" />
-        <div className="card-body">
-            <h5 className="card-title"> {title} </h5>
-            <p className="card-text"> Precio: {price} </p>
-            <p> Descripción: {description}</p>
-            <p> Stock Disponible: {stock}</p>
-            <div>
-            <ItemCount initial={1} stock={stock} onAdd={(quantity) => console.log('cantidad agregada', quantity)} />
+            <img src={image} className="card-img-top w-25" alt="title" />
+            <div className="card-body">
+                <h5 className="card-title"> {title} </h5>
+                <p className="card-text"> Precio: {price} </p>
+                <p> Descripción: {description}</p>
+                <p> Stock Disponible: {stock}</p>
+                <footer>
+                    {
+                        quantityAdded > 0 ? (
+                            <Link to="/CartWidget" className="Option" > Terminar Compra  </Link>
+
+                        ) : (
+                            <ItemCount initial={1} stock={stock} onAdd={handleOnAdd} />
+                        )
+                    }
+
+                </footer>
             </div>
         </div>
-    </div>
-        
-        
+
+
 
     )
 }
